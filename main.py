44e9c14efe32d7a3ae298bffd8cc1378edb0a083
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-01 21:05:23 +0330

    Add generate shares method

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ class ShamirSecret:
     _coef = []
 
     def __init__(self, s, n, t):
-        self.s = s # The secret
-        self.n = n # The number of shares
-        self.t = t # The number of participants needed to reconstruct the secret
+        self.s = s  # The secret
+        self.n = n  # The number of shares
+        self.t = t  # The number of participants needed to reconstruct the secret
 
         # Generate the coefficients of the polynomial
-        for i in range(t):
+        for _ in range(t):
             self._coef.append(random.randint(1, 1000))
 
+    # Generate the shares
+
+    def generate_shares(self):
+        shares = []
+        for xi in range(1, self.n + 1):
+            share = self.s
+            for pow_i in range(1, self.t):
+                share += self._coef[pow_i - 1] * xi ** pow_i
+
+            shares.append((xi, share))
+
+        return shares
