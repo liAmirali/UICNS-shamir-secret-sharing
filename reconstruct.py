1a71578e513cfe6e1e0eb9b7480aa8becc54491f
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-01 21:48:17 +0330

    Add lagrange interpolation code

--- reconstruct.py
+++ reconstruct.py
@@ -4,3 +4,16 @@ class reconstruct_shamir_secret:
         self.n = n
         self.p = p
         self.shares = shares
+
+    def _lagrange_interpolation(self, input, x, y):
+        result = 0
+        for i in range(len(y)):
+            term = 1
+            for j in range(len(y)):
+                if i == j:
+                    continue
+                term *= (input -  x[j]) / (x[i] - x[j])
+            term *= y[i]
+            result = (result + term) % self.p
+
+        return result
