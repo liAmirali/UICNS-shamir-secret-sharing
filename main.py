99a4b33aa9373eb575661679a35b3c413fb29c14
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-01 21:22:55 +0330

    Add p to the code

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import random
 class ShamirSecret:
     _coef = []
 
-    def __init__(self, s, n, t):
-        self.s = s  # The secret
-        self.n = n  # The number of shares
-        self.t = t  # The number of participants needed to reconstruct the secret
+    def __init__(self, s, n, t, p):
+        self.s = s % p  # The secret
+        self.n = n      # The number of shares
+        self.t = t      # The number of participants needed to reconstruct the secret
+        self.p = p      # The mode of numbers
 
         # Generate the coefficients of the polynomial
         for _ in range(t - 1):
-            self._coef.append(random.randint(1, 1000))
+            self._coef.append(random.randint(1, p))
 
     def _print_polynomial(self):
         print(f"f(x) = {self.s}", end="")
@@ -27,12 +28,12 @@ class ShamirSecret:
             for pow_i in range(1, self.t):
                 share += self._coef[pow_i - 1] * xi ** pow_i
 
-            shares.append((xi, share))
+            shares.append((xi, share % self.p))
 
         return shares
 
 
-shamir_secret = ShamirSecret(1005, 10, 6)
+shamir_secret = ShamirSecret(1005, 10, 6, 100)
 shamir_secret._print_polynomial()
 shares = shamir_secret.generate_shares()
 print(shares)
