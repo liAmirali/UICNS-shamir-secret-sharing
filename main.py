f2f9f1081172acad3cfe71cee5a4b8280eb67d75
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-07-01 21:15:38 +0330

    Add utility class to print the polynomial

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ class ShamirSecret:
         self.t = t  # The number of participants needed to reconstruct the secret
 
         # Generate the coefficients of the polynomial
-        for _ in range(t):
+        for _ in range(t - 1):
             self._coef.append(random.randint(1, 1000))
 
-    # Generate the shares
+    def _print_polynomial(self):
+        print(f"f(x) = {self.s}", end="")
+        for i in range(1, self.t):
+            print(f" + {self._coef[i-1]}x^{i}", end="")
+        print()
 
+    # Generate the shares
     def generate_shares(self):
         shares = []
         for xi in range(1, self.n + 1):
@@ -25,3 +30,9 @@ class ShamirSecret:
             shares.append((xi, share))
 
         return shares
+
+
+shamir_secret = ShamirSecret(1005, 10, 6)
+shamir_secret._print_polynomial()
+shares = shamir_secret.generate_shares()
+print(shares)
